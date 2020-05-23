fcbfb750124829bdbc31db5b9b8a3c59b50b4910
EC2 Default User <ec2-user@ip-172-31-84-218.scenthound.fart>
2020-05-23 21:52:50 +0000

    Cleaned up comments and files

--- calculator-client/main.go
+++ calculator-client/main.go
@@ -5,9 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"bytes"
-//	"encoding/json"
 	"github.com/golang/protobuf/proto"
-//	b64 "encoding/base64"
 	pb "scenthound/calculator/calculator"
 )
 
@@ -18,30 +16,13 @@ func main() {
 		Name: "Hendrix",
 	}
 
-	// In theroy, out is now a byte array that we should be able to pass to the post request
+	// Marshal our protobuf object into a byte array
 	requestBody, err := proto.Marshal(helloRequest)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	/*
-	// Base64 Encode
-	encodedRequest := b64.StdEncoding.EncodeToString(requestBody)
-	log.Print(encodedRequest)
-
-	// Base64 Decode
-	decodedRequest, _ := b64.StdEncoding.DecodeString(encodedRequest)
-
-	// Unmarshal
-	helloagain := &pb.HelloRequest{}
-	if err := proto.Unmarshal(decodedRequest, helloagain); err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Println(helloagain)
-	*/
-
-	// Create an http client so we can set custom headers
+	// Create an http client and set custom headers
 	client := http.Client{}
 	request, err := http.NewRequest("POST", "https://6xazvglpaj.execute-api.us-east-1.amazonaws.com/dev/grpc", bytes.NewBuffer(requestBody))
 	request.Header.Set("Content-Type", "application/x-protobuf")
@@ -56,41 +37,19 @@ func main() {
 		log.Fatalln(err)
 	}
 
-
-	// Curl the API gateway endpoing
-	/*	resp, err := http.Post("https://6xazvglpaj.execute-api.us-east-1.amazonaws.com/dev/grpc", "application/x-protobuf", bytes.NewBuffer(requestBody))
-	if err != nil {
-		log.Println("after post")
-		log.Fatalln(err)
-	}*/
-
-	/* Older JSON Marshaling
-	requestBody, err := json.Marshal(map[string]string{"name": "Hendrix",})
-
-	// Curl the API gateway endpoint
-	resp, err := http.Post("https://jxzyjaatkk.execute-api.us-east-1.amazonaws.com/dev/hello", "application/json", bytes.NewBuffer(requestBody))
-	if err != nil {
-		log.Fatalln(err)
-	}*/
-
 	defer resp.Body.Close()
 
 	helloReply := &pb.HelloReply{}
 
-	// Grab the message body
+	// Read the response
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Println("grabbing message body")
 		log.Fatalln(err)
 	}
 
 	if err := proto.Unmarshal(respBody, helloReply); err != nil {
-		log.Println(respBody)
-		log.Println("unmarshaling")
 		log.Fatalln(err)
 	}
 
-	// Print the message body
-//	log.Println(string(body))
 	log.Println(helloReply)
 }
--- calculator-grpc/main.go
+++ calculator-grpc/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"fmt"
-//	"bytes"
 	"context"
 	b64 "encoding/base64"
-	//"encoding/json"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/aws/aws-lambda-go/events"
@@ -14,17 +11,12 @@ import (
 )
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-//	var buf bytes.Buffer
-	//var msg string
 	var requestBody []byte
 
 	// Decode the base64 encoded message
 	if req.IsBase64Encoded {
-		//msg = "Message is Base64Encoded"
 		requestBody, _ = b64.StdEncoding.DecodeString(req.Body)
-	}// else {
-		//msg = "Message is NOT Base64Encoded"
-	//}
+	}
 
 	// Unmarshal back to protobuf
 	helloRequest := &pb.HelloRequest{}
@@ -37,16 +29,13 @@ func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.API
 		Message: helloRequest.Name + " is a good boy.",
 	}
 
-	fmt.Println(helloRequest.Name + " is a good boy.")
-
 	// Marshal response to binary
 	responseBody, err := proto.Marshal(helloReply)
 	if err != nil {
 		return events.APIGatewayProxyResponse{StatusCode: 404}, err
 	}
 
-	fmt.Println(responseBody)
-
+	// Ensure the body is Base64 encoded and Content-Type is set to 'application/x-protobuf'
 	resp := events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		IsBase64Encoded: true,
@@ -56,28 +45,9 @@ func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.API
 		},
 	}
 
-/*	body, err := json.Marshal(map[string]interface{}{
-		"message": msg,
-	})
-	if err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: 404}, err
-	}
-	json.HTMLEscape(&buf, body)
-
-	resp := events.APIGatewayProxyResponse{
-		StatusCode:	 200,
-		IsBase64Encoded: false,
-		Body:		 buf.String(),
-		Headers: map[string]string{
-			"Content-Type": "application/json",
-			"X-MyCompany-Func-Reply": "hello-handler",
-		},
-	}
-*/
 	return resp, nil
 }
 
 func main() {
 	lambda.Start(Handler)
 }
-
